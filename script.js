016dac6ca76d8ecf5bddf3bf23fb096826b3de75
samj31304-commits <samj31304@gmail.com>
2025-10-24 16:57:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.getElementById('clickBtn').addEventListener('click', () => {
   const message = document.getElementById('message');
-  message.textContent = 'Ambalvi Arain honay par hum Mian Faizan Farooqi Sahib ko bohat mubarak baad paish kartay hain!';
+  message.textContent = 'Kya aap ki toothpaste main namak hai?';
 });
